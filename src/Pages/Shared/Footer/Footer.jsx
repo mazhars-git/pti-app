1ff4f8e951e83a4d97a522b5133e7625197c0feb
mazhar <mazharulislam611@gmail.com>
2024-01-10 19:35:32 +0600

    update

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,34 +1,32 @@
 import footerIm from '../../../assets/images/image2.png';
 import { TiSocialTwitter } from "react-icons/ti";
 import { SlSocialInstagram } from "react-icons/sl";
-import { SlSocialGoogle } from "react-icons/sl";
+import { FaGoogle } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <footer className="footer p-10 bg-orange-400 text-base-content">            
+        <footer className="footer p-10 bg-orange-400 text-base-content">
             <nav className="mt-10">
                 <div>
-                    <header>
-                        <input type="email" className="input input-bordered input-md w-96" placeholder='Enter Your Email' name="" id="" />
-                        <button className="btn btn-rounded">Subscribe</button>
-                    </header>
-                    
+                    <div>
+                        <input type="email" className="input input-bordered input-md w-96 rounded-2xl" placeholder='Enter Your Email' name="" id="" />
+                        <button className="btn rounded-2xl">Subscribe</button>
+                    </div>
                 </div>
-                
-                
-                <div className="grid grid-cols-2">
+
+                <div className="grid grid-cols-2 gap-2">
                     <nav className="mt-20">
-                        <header className="footer-title text-4xl lowercase">pti.</header>
+                        <header className="footer-title text-4xl lowercase pb-2">pti.</header>
                         <p className="">Copyright&copy;Tripp.All Right Reserved</p>
                     </nav>
-                    <nav className="mt-24">
-                        <ul className='flex gap-2'>
-                            <li><a className="link link-hover"><SlSocialGoogle /></a></li>
-                            <li><a className="link link-hover"><TiSocialTwitter /></a></li>
-                            <li><a className="link link-hover"><SlSocialInstagram /></a></li>
+                    <nav className="mt-28">
+                        <ul className='flex gap-2 justify-end'>
+                            <li className='bg-slate-300 p-3 rounded-full cursor-pointer'><a className="link link-hover text-orange-600"><FaGoogle /></a></li>
+                            <li className='bg-slate-300 p-3 rounded-full cursor-pointer'><a className="link link-hover text-orange-600"><TiSocialTwitter /></a></li>
+                            <li className='bg-slate-300 p-3 rounded-full cursor-pointer'><a className="link link-hover text-orange-600"><SlSocialInstagram /></a></li>
                         </ul>
                     </nav>
-                </div>                               
+                </div>
             </nav>
 
             <aside>
