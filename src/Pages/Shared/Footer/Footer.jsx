6d130dbd2a4ec2b7e0d195eb361cccb8a1b0108f
mazhar <mazharulislam611@gmail.com>
2024-01-11 22:41:02 +0600

    update

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -7,7 +7,7 @@ import './Footer.css';
 const Footer = () => {
     return (
         <footer className="bg-amber-500">
-            <div className="max-w-screen-lg mx-auto footer p-10 text-base-content">
+            <div className="flex justify-between max-w-screen-lg mx-auto footer text-base-content">
                 <nav className="mt-10">
                     <div>
                         <div>
@@ -16,13 +16,13 @@ const Footer = () => {
                         </div>
                     </div>
 
-                    <div className="hero-content grid grid-cols-2 gap-2">
+                    <div className="flex flex-col-2 gap-10">
                         <nav className="mt-20">
                             <header className="footer-title text-4xl lowercase pb-2 text-black">pti<span className='text-orange-600'>.</span></header>
                             <p className="font-semibold text-md">Copyright&copy;Tripp.All Right Reserved</p>
                         </nav>
                         <nav className="mt-28">
-                            <ul className='flex gap-2 justify-end'>
+                            <ul className='flex gap-2 justify-end md:justify-center'>
                                 <li className='bg-slate-200 p-3 rounded-full cursor-pointer'><a className="link link-hover text-orange-500"><FaGoogle /></a></li>
                                 <li className='bg-slate-200 p-3 rounded-full cursor-pointer'><a className="link link-hover text-orange-500"><TiSocialTwitter /></a></li>
                                 <li className='bg-slate-200 p-3 rounded-full cursor-pointer'><a className="link link-hover text-orange-500"><SlSocialInstagram /></a></li>
