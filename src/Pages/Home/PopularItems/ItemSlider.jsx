33fc503fb827ef26bc70dea72362da1ddbefd562
mazhar <mazharulislam611@gmail.com>
2024-01-17 23:00:36 +0600

    desing deone

--- src/Pages/Home/PopularItems/ItemSlider.jsx
+++ src/Pages/Home/PopularItems/ItemSlider.jsx
@@ -18,7 +18,24 @@ const ItemSlider = () => {
         infinite: true,
         speed: 400,
         slidesToShow: 5,
-        slidesToScroll: 4
+        slidesToScroll: 5,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+        ]
     };    
     return (
         <Slider {...settings}>
